74c2da030ad367c61f21daa54948a5e2f080c43f
KeShih <self.ke.shi@gmail.com>
2024-11-19 21:34:16 +0800

    update

--- finger-tree.hs
+++ finger-tree.hs
@@ -1,4 +1,5 @@
 {-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+
 {-# HLINT ignore "Eta reduce" #-}
 
 import Data.Binary.Get (isEmpty)
@@ -319,6 +320,15 @@ merge (OrdSeq xs) (OrdSeq ys) = OrdSeq (merge' xs ys)
         where
           (l, r) = split (> measure y) xs
 
+intersect :: (Ord a) => OrdSeq a -> OrdSeq a -> OrdSeq a
+intersect (OrdSeq xs) (OrdSeq ys) = OrdSeq (intersect' xs ys)
+  where
+    intersect' xs ys = case viewL ys of
+      NilL -> Empty
+      ConsL y ys' -> case lookupTree (>= measure y) (#) xs of
+        (NoKey, _) -> intersect' xs ys'
+        (k, x) -> x <| intersect' (dropUntil (>= k) xs) ys'
+
 -- test OrdSeq
 
 testOrdSeq :: IO ()
@@ -345,6 +355,7 @@ testOrdSeq = do
   print xs9
   let ys = OrdSeq $ toTree [Elem 1, Elem 1, Elem 4, Elem 5, Elem 14]
   print $ merge xs ys
+  print $ intersect xs ys
 
 testSeq :: IO ()
 testSeq = do
